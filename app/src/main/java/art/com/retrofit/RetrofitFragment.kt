39464ba44d758c812b9417b976c1e56f25eba698
artur <artur.arciuch@gmail.com>
2022-04-22 13:30:17 +0200

    c1

--- app/src/main/java/art/com/retrofit/RetrofitFragment.kt
+++ app/src/main/java/art/com/retrofit/RetrofitFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import art.com.R
 import art.com.databinding.FragmentRetrofitBinding
 
@@ -12,6 +14,7 @@ import art.com.databinding.FragmentRetrofitBinding
 class RetrofitFragment : Fragment() {
 
     private lateinit var binding : FragmentRetrofitBinding
+    private lateinit var retrofitViewModel: RetrofitViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +26,13 @@ class RetrofitFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding =
-        return inflater.inflate(R.layout.fragment_retrofit, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_retrofit, container, false)
+        retrofitViewModel = ViewModelProvider(this).get(RetrofitViewModel::class.java)
+        binding.viewModel = retrofitViewModel
+        //binding.lifecycleOwner = viewLifecycleOwner
+
+        return binding.root
     }
 
 }
--- app/src/main/java/art/com/retrofit/RetrofitViewModel.kt
+++ app/src/main/java/art/com/retrofit/RetrofitViewModel.kt
@@ -1,7 +1,43 @@
 package art.com.retrofit
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class RetrofitViewModel : ViewModel(){
+class RetrofitViewModel : ViewModel() {
+    private val RVM= "RETROFITVIEWMODEL"
 
+    private var _response : MutableLiveData<String> = MutableLiveData()
+    val response : LiveData<String>
+        get() = _response
+
+    init {
+        Log.i(RVM, "RetrofitViewModel init")
+        getMarsRealEstateProperties()
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i(RVM, "RetrofitViewModel onCleared")
+    }
+
+    private fun getMarsRealEstateProperties() {
+        //_response.value = "Set the Mars API Response here!"
+        MarsApi.retrofitService.getProperties().enqueue(
+            object : Callback<String> {
+                override fun onResponse(call: Call<String>, response: Response<String>) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onFailure(call: Call<String>, t: Throwable) {
+                    TODO("Not yet implemented")
+                }
+            }
+        )
+    }
 }
